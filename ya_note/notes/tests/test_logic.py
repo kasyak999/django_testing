4b7a92e9ec40a00dcfa1e65a2ed4e3aacc8393c1
Maksim <kasyak999@yandex.ru>
2024-07-27 23:15:06 +0300

    up

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -2,8 +2,9 @@ from django.test import TestCase, Client
 from notes.models import Note
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from notes.forms import NoteForm
+from notes.forms import WARNING
 from http import HTTPStatus
+from pytils.translit import slugify
 
 
 User = get_user_model()
@@ -62,14 +63,46 @@ class TestDetailPage(TestCase):
         client.post(reverse('notes:add'), data=self.form_data)
         comments_count = Note.objects.count()
         self.assertEqual(comments_count, initial_count)
-    
+
+    def test_impossible_to_create_identical_slag(self):
+        """Невозможно создать две заметки с одинаковым slug."""
+        initial_count = Note.objects.count()
+        self.form_data['slug'] = self.COMMENT_TEXT[2]
+        response = self.auth_client.post(
+            reverse('notes:add'), data=self.form_data
+        )
+        self.assertFormError(
+            response,
+            form='form',
+            field='slug',
+            errors=self.form_data['slug'] + WARNING
+        )
+        count = Note.objects.count()
+        self.assertEqual(initial_count, count)
+
+    def test_processing_slug(self):
+        """
+        Если при создании заметки не заполнен slug, то он формируется
+        автоматически, с помощью функции pytils.translit.slugify.
+        """
+        count = Note.objects.count()
+        self.form_data['slug'] = ''
+        self.auth_client.post(
+            reverse('notes:add'), data=self.form_data
+        )
+        count_2 = Note.objects.count()
+        self.assertEqual(count_2, count + 1)
+        created_note = Note.objects.get(title=self.form_data['title'])
+        slug = slugify(self.form_data['title'])[:100]
+        self.assertEqual(created_note.slug, slug)
+
     def test_not_author_can_delete(self):
         """Пользователь не может удалять чужие заметки"""
         response = self.reader_client.delete(self.delete_url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         comments_count = Note.objects.count()
         self.assertEqual(comments_count, 1)
-    
+
     def test_not_author_can_edit(self):
         """Пользователь может редактировать чужие заметки"""
         response = self.reader_client.post(
@@ -80,7 +113,7 @@ class TestDetailPage(TestCase):
         self.assertNotEqual(self.note.title, self.NEW_COMMENT_TEXT[0])
         self.assertNotEqual(self.note.text, self.NEW_COMMENT_TEXT[1])
         self.assertNotEqual(self.note.slug, self.NEW_COMMENT_TEXT[2])
-    
+
     def test_author_can_delete(self):
         """Пользователь может удалять свои заметки"""
         response = self.auth_client.delete(self.delete_url)
